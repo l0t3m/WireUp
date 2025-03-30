eca506200918a86341c67320a849110b1b03926c
lotem <lotem1shriker@gmail.com>
2025-03-30 16:40:30 +0300

    Improved ui

--- UAFinal/Assets/Scripts/Handlers/TutorialHandler.cs
+++ UAFinal/Assets/Scripts/Handlers/TutorialHandler.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class TutorialHandler : MonoBehaviour
 {
     [SerializeField] GameObject[] TutorialParts;
+    [SerializeField] VideoClip[] VideoClips;
+    [SerializeField] RenderTexture[] VideoRenderTexture;
+    [SerializeField] GameObject VideoManager;
+    [SerializeField] RawImage RawImage;
     private int currentPart = 0;
 
     private void Start()
@@ -11,15 +18,40 @@ public class TutorialHandler : MonoBehaviour
             TutorialParts[currentPart].SetActive(true);
         else
             gameObject.SetActive(false);
+
+        if (VideoClips[currentPart] != null)
+        {
+            RawImage.gameObject.SetActive(true);
+            VideoManager.GetComponent<VideoPlayer>().clip = VideoClips[currentPart];
+            VideoManager.GetComponent<VideoPlayer>().targetTexture = VideoRenderTexture[currentPart];
+            RawImage.texture = VideoRenderTexture[currentPart];
+        }
     }
 
     public void ShowNext()
     {
         if (currentPart < TutorialParts.Length-1)
         {
+
             TutorialParts[currentPart].SetActive(false);
             currentPart++;
             TutorialParts[currentPart].SetActive(true);
+
+            if (VideoClips[currentPart] != null)
+            {
+                RawImage.gameObject.SetActive(true);
+                VideoManager.GetComponent<VideoPlayer>().clip = VideoClips[currentPart];
+                VideoManager.GetComponent<VideoPlayer>().targetTexture = VideoRenderTexture[currentPart];
+                RawImage.texture = VideoRenderTexture[currentPart];
+            }
+            else
+            {
+                VideoManager.GetComponent<VideoPlayer>().enabled = false;
+                RawImage.gameObject.SetActive(false);
+            }
+
+            
+
         }
         else
         {
