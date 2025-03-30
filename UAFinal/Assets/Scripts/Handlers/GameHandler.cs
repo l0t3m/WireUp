afa6d5e540f1494ccea5ae8ffbc67ad018fe3585
ZeUnlucky <tohar77@hotmail.com>
2025-03-30 04:22:17 +0300

    c

--- UAFinal/Assets/Scripts/Handlers/GameHandler.cs
+++ UAFinal/Assets/Scripts/Handlers/GameHandler.cs
@@ -31,10 +31,7 @@ public class GameHandler : MonoBehaviour
     private float idleTime = 5;
     [SerializeField] float idleTimeBase = 5;
     private Vector3 previousPosition;
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 12f7da8 (added new feature)
 
     private void Start()
     {
